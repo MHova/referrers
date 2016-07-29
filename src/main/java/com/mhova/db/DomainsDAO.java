a62c19fd646fca83dc1629e8acd2a7836c4f9399
Michael Ho <mho@sumall.com>
2016-07-28 18:47:48 -0700

    Use transactions when upserting and getting count Which also makes the DomainsDAO interface more high-level

--- src/main/java/com/mhova/db/DomainsDAO.java
+++ src/main/java/com/mhova/db/DomainsDAO.java
@@ -5,21 +5,28 @@ import java.util.List;
 import org.skife.jdbi.v2.sqlobject.Bind;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
 import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+import org.skife.jdbi.v2.sqlobject.Transaction;
 import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
 
 import com.mhova.api.DomainSightings;
 
 @RegisterMapper(DomainSightingsMapper.class)
-public interface DomainsDAO {
+public abstract class DomainsDAO {
+    @Transaction
+    public int recordSighting(final String domain) {
+        insertOrIncrementCount(domain);
+        return getCount(domain);
+    }
+
     @SqlUpdate("insert into domains as r (domain, count) values (:domain, 1)"
-            + "on conflict (domain)"
-            + "do update set count = r.count + 1"
-            + "where r.domain = :domain")
-    void insertOrIncrementCount(@Bind("domain") final String domain);
+               + "on conflict (domain)"
+               + "do update set count = r.count + 1"
+               + "where r.domain = :domain")
+    protected abstract void insertOrIncrementCount(@Bind("domain") final String domain);
 
     @SqlQuery("select count from domains where domain = :domain")
-    int getCount(@Bind("domain") final String domain);
+    protected abstract int getCount(@Bind("domain") final String domain);
 
     @SqlQuery("select domain, count from domains order by count desc limit 3")
-    List<DomainSightings> getTopThreeDomains();
+    public abstract List<DomainSightings> getTopThreeDomains();
 }
